4698fa5bd1014b5cd1ad032463169e46eef86bd6
Afthash Sahal Ubaid Puzhakkal <afthashsahal123@gmail.com>
2024-10-09 16:14:57 +1030

    Improved home section and started working on about section

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-window.addEventListener("scroll", function() {
+let firstScroll = false;
+let linkClicked = false;
+
+const links = document.querySelectorAll("#navbar a");
+
+links.forEach(link => {
+  link.addEventListener("click", function() {
+    linkClicked = true;
+    firstScroll = true;
+  })
+})
+
+window.addEventListener("scroll", function(event) {
   const home = document.querySelector("#home");
-  const navbar = document.querySelector("#navbar");
+  const about = document.querySelector("#about");
+  const progressBar = document.querySelector(".progress-bar");
   const scrollPosition = window.scrollY;
 
-  if (scrollY) {
+  if (!firstScroll && !linkClicked) {
+    event.preventDefault();
+    window.scrollTo({
+      top: home.offsetTop,
+      behavior: "smooth"
+    });
+    firstScroll = true;
+  }
+
+  if (firstScroll && scrollY) {
     document.body.classList.add("scrolled");
   } else {
     document.body.classList.remove("scrolled");
+    firstScroll = false;
+    linkClicked = false;
   }
+
+  const aboutTop = about.offsetTop;
+  const aboutHeight = about.offsetHeight;
+  const aboutScrollPosition = window.scrollY + window.innerHeight;
+
+  const progress = Math.min((aboutScrollPosition - aboutTop) / aboutHeight, 1);
+  progressBar.style.height = `${progress * 100}%`;
 })
